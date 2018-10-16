7c62fcb20d14df135b97f0b4a4808d90d0d52ac1
Saytes <saytes@correo.ugr.es>
2018-10-16 11:05:46 +0200

    añadida integración pytest y el entorno virtual

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='travistest',
+    packages=['travistest'],
+    version='0.0.1',
+    description='Testeando los hitos del proyecto Birthbot con TravisCI',
+    url='https://github.com/Saytes/BirthBot.git',    
+)
