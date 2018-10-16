5187de5246be81fb2ebc3e8fffabb1e69cdac594
Saytes <saytes@correo.ugr.es>
2018-10-16 11:27:50 +0200

    Finalizada integracion con Travis Closes #6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='travistest',
     packages=['travistest'],
-    version='0.0.1',
+    version='1.0.0',
     description='Testeando los hitos del proyecto Birthbot con TravisCI',
     url='https://github.com/Saytes/BirthBot.git',    
 )
