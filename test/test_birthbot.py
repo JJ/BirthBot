08a128c6f19c02805bdacee2ff4cda54eb304be0
Saytes <saytes@correo.ugr.es>
2018-10-16 11:04:36 +0200

    Terminado test clase

--- test/test_birthbot.py
+++ test/test_birthbot.py
@@ -2,11 +2,12 @@ import datetime
 
 from pytest import fixture
 
+
 @fixture
 
 def op():
     from travistest.birthbot import Birthday
-    return Birthday(datetime.date(1995,11,25), "Jorge Gutiérrez Segovia")
+    return Birthday(datetime.date(1995,11,25), "Jorge Gutierrez Segovia")
 
 def test_birt_date(op):
     assert op.get_birth_date()
